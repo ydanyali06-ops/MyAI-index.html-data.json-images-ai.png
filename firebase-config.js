27069dbf4181059e8689399dc10def03daa39ec0
ydanyali06-ops <ydanyali06@gmail.com>
2026-09-18 01:58:36 +0330

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-
+export const firebaseConfig = {
+  apiKey: "AIzaSyBrMw31xVAszqXUvP6AiufOGL_6XdYoaM4",
+  authDomain: "myai-bd693.firebaseapp.com",
+  projectId: "myai-bd693",
+  storageBucket: "myai-bd693.firebasestorage.app",
+  messagingSenderId: "305975730704",
+  appId: "1:305975730704:web:5a5ac34ae4493faca06095",
+  measurementId: "G-Z7ZPK7SKHM"
+};
