5f51ffc58eb4ddcf11bb549adc08fd12fd2922ea
ydanyali06-ops <ydanyali06@gmail.com>
2026-09-18 01:40:46 +0330

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,1 @@
-// firebase-config.js
 
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
